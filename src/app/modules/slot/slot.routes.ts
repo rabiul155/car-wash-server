84af62cceac7e0d3e31f023ccdeed12ce05ab5fb
rabiul155 <siyamict@gmail.com>
2024-07-28 20:31:00 +0600

    add authentication in slot

--- src/app/modules/slot/slot.routes.ts
+++ src/app/modules/slot/slot.routes.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import validateRequest from '../../utils/validateRequest';
 import slotValidationSchema from './slot.validate';
 import { slotController } from './slot.controllers';
+import { authenticate, authorization } from '../../middleware/auth';
 
 const router = express.Router();
 
 router.post(
   '/',
+  authenticate,
+  authorization('admin'),
   validateRequest(slotValidationSchema),
   slotController.createSlot,
 );
