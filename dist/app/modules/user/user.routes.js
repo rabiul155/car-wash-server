e7b152a8a27b6cbb1ea3c58d9861a51091a042e3
rabiul155 <siyamict@gamil.com>
2024-08-11 11:02:08 +0600

    fix signup spelling issue

--- dist/app/modules/user/user.routes.js
+++ dist/app/modules/user/user.routes.js
@@ -10,7 +10,7 @@ const user_controllers_1 = __importDefault(require("./user.controllers"));
 const auth_controllers_1 = __importDefault(require("../auth/auth.controllers"));
 const validateRequest_1 = __importDefault(require("../../utils/validateRequest"));
 const router = express_1.default.Router();
-router.post('/singup', (0, validateRequest_1.default)(user_validate_1.default), auth_controllers_1.default.singUpUser);
+router.post('/signup', (0, validateRequest_1.default)(user_validate_1.default), auth_controllers_1.default.singUpUser);
 router.post('/login', auth_controllers_1.default.logInUser);
 router.route('/').get(user_controllers_1.default.getUsers);
 exports.userRoutes = router;
